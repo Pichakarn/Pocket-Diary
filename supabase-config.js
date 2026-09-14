41c5d40690c076beffaef6bb81f996eb9aa8c00e
Pichakarn <pichayakanchivapimrak@gmail.com>
2026-09-15 00:19:33 +0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = 'https://ooozqddxvxeykwubumli.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_xwsBeo4TJfLSwnQUiV2uSw_7GwLZd85';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
